812948ac80659b6ab2a7d1f57972feef78d95173
Abrarul Rhythm <[....email>
2025-08-27 22:53:54 +0600

    added call history container data load functionality

--- js/script.js
+++ js/script.js
@@ -52,5 +52,24 @@ for (const callButton of callButtons) {
         }
 
         callHistoryData.push(historyData);
+
+        // History Record
+        const historyContainer = document.getElementById('all-history-wrap');
+        historyContainer.innerText = "";
+
+        for (const data of callHistoryData) {
+            const div = document.createElement('div');
+            div.innerHTML = `
+                <div class="bg-[#FAFAFA] p-4 rounded-lg flex justify-between items-center mb-2">
+                    <div>
+                        <h1 class="font-Inter text-lg font-semibold mb-1">${data.serviceTitle}</h1>
+                        <span>${data.serviceNumber}</span>
+                    </div>
+                    <div class="time">${data.date}</div>
+                </div>
+            `;
+
+            historyContainer.appendChild(div);
+        }
     });
 }

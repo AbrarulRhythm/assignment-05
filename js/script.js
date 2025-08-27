645e52d0010c7f77abcebd384f77b8cf235f7f27
Abrarul Rhythm <[....email>
2025-08-27 18:08:16 +0600

    added call button and history functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const callHistoryData = [];
+
 // Function to get inner text
 function getInnerText(id) {
     const element = document.getElementById(id);
@@ -17,5 +19,38 @@ for (const heartButton of heartButtons) {
         const totalNewHeart = 1 + totalHeart;
 
         document.getElementById('heart-count').innerText = totalNewHeart;
+    });
+}
+
+// Call Button Functionality
+const callButtons = document.getElementsByClassName('call-button');
+
+for (const callButton of callButtons) {
+    callButton.addEventListener('click', function () {
+        const card = callButton.closest('.emergency-number-card');
+
+        const title = card.querySelector('.emergency-number-title').innerText;
+        const number = card.querySelector('.emergency-number').innerText;
+
+        const totalCoin = getInnerText('coin-count');
+
+        if (totalCoin < 20) {
+            alert("❌ You don't have enough coins. You need at least 20 coins to make a call.");
+            return;
+        }
+
+        const totalNewCoin = totalCoin - 20;
+
+        document.getElementById('coin-count').innerText = totalNewCoin;
+
+        alert(`📞 Calling ${title} ${number}...`);
+
+        const historyData = {
+            serviceTitle: title,
+            serviceNumber: number,
+            date: new Date().toLocaleTimeString()
+        }
+
+        callHistoryData.push(historyData);
     });
 }
